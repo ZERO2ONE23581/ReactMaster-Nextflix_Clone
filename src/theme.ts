9445def64b6531bd8812d53541df6f84d5742339
aiden <zero2one23581@gmail.com>
2022-04-16 23:05:43 +0900

    Set up

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  red: '#d63031',
+  black: {
+    veryDark: '#2d3436',
+    darker: '#353b48',
+    lighter: '#636e72',
+  },
+  white: {
+    darker: '#b2bec3',
+    lighter: '#dfe6e9',
+  },
+};
