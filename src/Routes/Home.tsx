af414a4e06dfd93fd8ab382392261eca51840fcd
aiden <zero2one23581@gmail.com>
2022-04-17 18:38:20 +0900

    Slider part two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -5,17 +5,20 @@ import styled from 'styled-components';
 import { getMovies, IgetMoviesResult } from '../api';
 import { makeImagePath } from '../utills';
 
+//Animation
 const rowVars = {
   hidden: {
-    x: window.outerWidth,
+    x: window.outerWidth + 7,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth,
+    x: -window.outerWidth - 7,
   },
 };
+
+const offset = 6; //movies you want to show in one time
 
 function Home() {
   const { data, isLoading } = useQuery<IgetMoviesResult>(
@@ -24,8 +27,19 @@ function Home() {
   );
   //Slider
   const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
   const increaseIndex = () => {
-    setIndex((prev) => prev + 1);
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      //빠르게 슬라이더 두번누를때 간격없애기 코드
+      const totalMovies = data.results.length - 1; //첫번째 row는 이미 쓰고있기 때문에 -1
+      const maxIndex = Math.floor(totalMovies / offset);
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
   };
   //
   return (
@@ -34,13 +48,13 @@ function Home() {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}>
+          <Banner bg={makeImagePath(data?.results[0].backdrop_path || '')}>
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <button onClick={increaseIndex}>INCREASE INDEX</button>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVars}
                 initial="hidden"
@@ -49,9 +63,15 @@ function Home() {
                 transition={{ type: 'tween', duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bg={makeImagePath(movie.backdrop_path || '', 'w500')}
+                    />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
@@ -61,12 +81,13 @@ function Home() {
   );
 }
 export default Home;
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bg: string }>`
+  background-image: url('${(p) => p.bg}');
+  background-size: cover;
+  background-position: center center;
   position: relative;
-  //
-  background-color: white;
   height: 200px;
-  font-size: 60px;
+  font-size: 20px;
   color: red;
 `;
 
@@ -75,7 +96,7 @@ const Row = styled(motion.div)`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  gap: 10px;
+  gap: 7px;
 `;
 
 const Slider = styled.div`
@@ -92,16 +113,16 @@ const Overview = styled.p`
   width: 60%;
 `;
 
-const Banner = styled.section<{ bgPhoto: string }>`
+const Banner = styled.section<{ bg: string }>`
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(p) => p.bgPhoto});
+    url('${(p) => p.bg}');
   background-size: cover;
   color: ${(p) => p.theme.white.lighter};
   padding: 60px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  height: 100vh;
+  height: 50vh;
 `;
 
 const Loader = styled.div`
