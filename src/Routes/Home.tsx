5ce4128a5dbc8bd1f27ba4d0347cb953a8b001fc
aiden <zero2one23581@gmail.com>
2022-04-17 17:16:22 +0900

    Slider part one

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,14 +1,32 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getMovies, IgetMoviesResult } from '../api';
 import { makeImagePath } from '../utills';
+
+const rowVars = {
+  hidden: {
+    x: window.outerWidth,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth,
+  },
+};
 
 function Home() {
   const { data, isLoading } = useQuery<IgetMoviesResult>(
     ['movies', 'nowPlaying'],
     getMovies
   );
-
+  //Slider
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => {
+    setIndex((prev) => prev + 1);
+  };
   //
   return (
     <Cont>
@@ -20,17 +38,55 @@ function Home() {
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <button onClick={increaseIndex}>INCREASE INDEX</button>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVars}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: 'tween', duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Cont>
   );
 }
 export default Home;
+const Box = styled(motion.div)`
+  position: relative;
+  //
+  background-color: white;
+  height: 200px;
+  font-size: 60px;
+  color: red;
+`;
+
+const Row = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+`;
+
+const Slider = styled.div`
+  top: -100px;
+`;
 
 const Title = styled.h2`
   font-size: 60px;
   margin-bottom: 20px;
 `;
+
 const Overview = styled.p`
   font-size: 25px;
   width: 60%;
